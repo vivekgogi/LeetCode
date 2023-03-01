bf4b97644c027999590c2d72c7c7b9b4f6c6ed85
VivekGogi <vivekgogi123vg@gmail.com>
2023-03-02 02:55:22 +0530

    02-03-2023

---
+++ March/2062-Count-Vowel-Substrings-of-a-String.cpp
@@ -0,0 +1,28 @@
+// 2062. Count Vowel Substrings of a String
+// https://leetcode.com/problems/count-vowel-substrings-of-a-string/
+
+class Solution
+{
+    bool isVowel(char c)
+    {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    };
+
+public:
+    int countVowelSubstrings(string s)
+    {
+        int ans = 0, N = s.size();
+        unordered_map<char, int> cnt;
+        for (int i = 0; i < N; ++i)
+        {
+            cnt.clear();
+            for (int j = i; j < N && isVowel(s[j]); ++j)
+            {
+                cnt[s[j]]++;
+                if (cnt.size() == 5)
+                    ++ans;
+            }
+        }
+        return ans;
+    }
+};
